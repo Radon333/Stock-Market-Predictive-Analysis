1131dd066cdb180234af174081ade8568533ff74
Radon333 <ranveer2001s@gmail.com>
2022-04-17 20:30:38 +0530

    landing page

--- Web/client/src/App.js
+++ Web/client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import hero from "./Components/images/landing.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:"center"}}>
+      <Typography mt={10} variant="h3" gutterBottom component="div" style={{color:"#161b22",fontFamily:"Fira Sans",fontWeight:"700"}}>
+        One & Zero
+      </Typography>
+      <div>
+      <img style={{maxHeight:"35vh"}} src={hero} alt="hero" />
+      </div>
+      <Typography mt={6} variant="h4" gutterBottom component="div" style={{color:"#424242",fontSize:"3em",fontFamily:"Fira Sans",fontWeight:"700"}}>
+        Investing Insights
+      </Typography>
+      <Typography  variant="subtitle1" gutterBottom component="div" style={{fontSize:"1.5em",color:"#424242",fontFamily:"Fira Sans",fontWeight:"500"}}>
+        AI powered platoform to help you make smart decisions before investing
+      </Typography>
+      <Button variant="contained" color="primary" size="large">Sign up now</Button>
+
     </div>
   );
 }
